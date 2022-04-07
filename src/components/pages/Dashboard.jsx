0ca3a7196c4cd971e162e6da06e294d795cc2610
Hasnataddo <hasnat150822@gmail.com>
2022-04-07 11:54:29 +0500

    git tutoorial

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,6 +1,11 @@
 let Dashboard = () => {
     return (
-        <h1>This is dashboard</h1>
+        <>
+            <h1>This is dashboard!</h1>
+            <p>Never do for your appraisal</p>
+            <p className="text-dark fw-bold">Do u know? Javascript provide XSS security be default.</p>
+            <strong className="text-success">Its amazing</strong>
+        </>
     )
 }
 
